5f204a78c96655c1e231e2358e3960ab9bc02e85
yousef <yabdelshafy0@gmail.com>
2023-03-22 01:35:20 +0200

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,15 @@
-#include<stdio.h>
 #include<main.h>
+
 int main(void)
 {
-printf("_putchar/n");
+_putchar("_");
+int _putchar("p");
+int _putchar("u");
+int _putchar("t");
+int _putchar("c");
+int _putchar("h");
+int _putchar("a");
+int _putchar("r");
 
 return(0);
 }
