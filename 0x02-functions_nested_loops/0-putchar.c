bb496f209ba249f4da7e9693019a957399a6b2be
yousef <yabdelshafy0@gmail.com>
2023-03-22 02:14:53 +0200

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,14 +2,14 @@
 
 int main(void)
 {
-int _putchar('_');
-int _putchar("p");
-int _putchar("u");
-int _putchar("t");
-int _putchar("c");
-int _putchar("h");
-int _putchar("a");
-int _putchar("r");
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 
 return(0);
 }
