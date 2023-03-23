35578574434051e564b6b9898178ac327e3a00fa
yousef <yabdelshafy0@gmail.com>
2023-03-23 03:19:00 +0200

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+/**
+* add - adds two integers and returns the result
+* @n1: first operand
+* @n2: second operand
+*
+* Return: The sum of the two parameters
+*/
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
