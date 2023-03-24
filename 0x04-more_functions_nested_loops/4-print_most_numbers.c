f370acabd506a5ce93bb446450b2424329293b8f
yousef <yabdelshafy0@gmail.com>
2023-03-24 23:45:12 +0200

    apdate

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,7 +11,9 @@ int i;
 for (i = 0; i <= 9; i++)
 {
 	if (i != 2 && i != 4)
+	{
 		_putchar(i + 48);
+	}
 }          
 _putchar('\n'); 
 }
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,14 @@
+#include"main.h"
+
+
+void more_numbers(void)
+{
+int i;
+int n;
+for (i = 0; i < 10; i++)
+{
+if (n >= 0 && n <= 14)
+       _putchar(n);	
+		}
+_putchar('\n');
+}
