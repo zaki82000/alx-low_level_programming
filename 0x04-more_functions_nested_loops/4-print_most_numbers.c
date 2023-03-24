0e2abfa48bc9b09add77f32c8e71dcf82983e249
yousef <yabdelshafy0@gmail.com>
2023-03-24 04:29:29 +0200

    most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include"main.h"
+/**
+ *print_most_numbers - function that prints the numbers, from 0 to 9.
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i != 2 && i != 4)
+			_puutchar(i + 48);
+	}
+
+	_putchar('\n');
+}
