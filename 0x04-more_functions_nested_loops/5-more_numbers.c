868f2e2c40af9761b9a9a2070b25880794ad0030
yousef <yabdelshafy0@gmail.com>
2023-03-25 01:04:57 +0200

    more

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,10 +9,13 @@ int n, i;
 for (i = 0; i < 10; i++)
 {
 for (n = 0; n <= 14; n++)
+{
+	if (n >= 10)
 
-_putchar((n % 10) + 48);
+_putchar(1 + 48);
 
+	_putchar((n % 10) + 48);
 }
 _putchar('\n');
-
+}
 }
