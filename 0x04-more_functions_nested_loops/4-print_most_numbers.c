db5200675141e3e51b09e92f43c35be6d19bfaee
yousef <yabdelshafy0@gmail.com>
2023-03-24 14:46:24 +0200

    apdate

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,15 +2,16 @@
 /**
  *print_most_numbers - function that prints the numbers, from 0 to 9.
  */
+
 void print_most_numbers(void)
-{	
-	int i;
+{
 
-	for (i = 0; i <= 9; i++)
-	{
-		if (i != 2 && i != 4)
-			_putchar(i + 48);
-	}
+int i;
 
-	_putchar('\n');
+for (i = 0; i <= 9; i++)
+{
+	if (i != 2 && i != 4)
+		_putchar(i + 48);
+}          
+_putchar('\n'); 
 }
