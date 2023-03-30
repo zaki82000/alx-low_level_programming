03f93bc72e1c859c37d09e5a243ec0cf2e66cedb
yousef <yabdelshafy0@gmail.com>
2023-03-31 01:30:12 +0200

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,26 +5,13 @@ char *_strncpy(char *dest, char *src, int n)
 
 {
 
-	char *d_temp = dest;
-	char *s_temp = src;
+	n = 0;
+	while (src[n] != '\0')
+	{
 
-	
-		while (*d_temp != '\0')
-
-		d_temp++;
-		s_temp++;
-	
-		if (n > 0)
-		{
-			while (*s_temp)
-			{
-				*d_temp++ = *s_temp++;
-			}
-			}
-
-		*d_temp= '\0';
-			return (dest);
-
-
-
+		dest[n] = src[n];
+	n++;
+	}
+	dest[n] = '\0';
+	return (dest);
 }
