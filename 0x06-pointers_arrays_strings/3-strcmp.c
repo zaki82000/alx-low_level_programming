657fb08969c212a1cba825315f99744f5d8dec5d
yousef <yabdelshafy0@gmail.com>
2023-03-31 04:22:54 +0200

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,19 +5,26 @@ int _strcmp(char *s1, char *s2)
 {
 	int a = 0;
 
-	while (*s1 != '\0' && *s2 != '\0')
+	while (*s1 != '\0' || *s2 != '\0')
 	{
-		if(*s1!=*s2)
+		if(*s1 == *s2)
 		{
-	a=1;
-		}
 		s1++;
-		s2++;
-	}
-	if (a==0)
-		return (0);
-	else
-		return(1);
+        	s2++;
+		}
 	
-
+		else if ((*s1 == '\0' && *s2 != '\0') || (*s1 != '\0' && *s2 == '\0') || (*s1 != *s2)) 
+		{
+			a = 1;
+			
+			
+	
+		}
+	}
+	if(a==0)
+	{
+    return 0;
+	}
+    else
+    return 1;
 }
