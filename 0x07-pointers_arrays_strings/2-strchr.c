9b6ea0f316e9665e0f2e9f8487cdaa3cbd0af260
yousef <yabdelshafy0@gmail.com>
2023-04-04 01:57:00 +0200

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,23 +10,21 @@
 char *_strchr(char *s, char c)
 
 {
-    
-    char *temp = s;
-   
 
-    while (*temp)
-    {
-        
-        if (*temp == c)
-        {
-            return(temp);
-        }
-        
-        temp++;
-    }
-    if (*temp != c)
-        {
-        return (0);
-        }
-	return(s);
+				char *temp = s;
+
+while (*temp)
+{
+if (*temp == c)
+{
+return (temp);
+}
+
+temp++;
+}
+if (*temp != c)
+{
+return (s);
+}
+return (0);
 }
