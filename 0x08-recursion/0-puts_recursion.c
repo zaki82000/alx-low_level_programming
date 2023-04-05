0ae22ee2da7fe78130bb4a2fa4a75270f7edd51c
yousef <yabdelshafy0@gmail.com>
2023-04-05 21:22:31 +0200

    puts

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,12 +6,12 @@
 */
 void _puts_recursion(char *s)
 {
- if (*s == 0)
- {
+if (*s == 0)
+{
 putchar ('\n');
- return;
- }      
+return;
+}
 putchar (*s);
- _puts_recursion(s+1);
+_puts_recursion(s + 1);
 
 }
