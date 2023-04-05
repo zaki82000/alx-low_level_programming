b1478a76fd125ce0841e83b2357f91259365a902
yousef <yabdelshafy0@gmail.com>
2023-04-06 01:41:00 +0200

    puts

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include"main.h"
+
+/**
+*_strlen_recursion - function that returns the length of a string.
+*@s: is a string.
+* Return: if *s != end string return 0.
+*/
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+	return (0);
+	}
+	return (1 + _strlen_recursion(s + 1));
+}
