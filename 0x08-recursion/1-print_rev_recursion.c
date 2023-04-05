b928f79e40355233a6615a85350b2cef9093e490
yousef <yabdelshafy0@gmail.com>
2023-04-05 23:38:07 +0200

    puts

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,17 +1,15 @@
 #include"main.h"
 
 /**
-*_puts_recursion - function that prints a string,i:Length of srting.
+*_print_rev_recursion - function that prints a string,i:Length of srting.
 *@s: string.
 */
 void _print_rev_recursion(char *s)
 {
-if (*s == 0)
-{
-return;
-}       
+	if (*s == 0)
+	return;
 
-_print_rev_recursion(s + 1);
-putchar (*s);
+	_print_rev_recursion(s + 1);
+	putchar (*s);
 }
 
