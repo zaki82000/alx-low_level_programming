662a24e36955a9b48deb894c5d6f89319a08c870
yousef <yabdelshafy0@gmail.com>
2023-04-05 21:26:54 +0200

    puts

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+*_puts_recursion - function that prints a string,i:Length of srting.
+*@s: string.
+*/
+void _print_rev_recursion(char *s)
+{
+if (*s == 0)
+{
+return;
+}       
+
+_puts_recursion(s + 1);
+putchar (*s);
+}
+
