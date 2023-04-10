cb7b4a56aa9a2cd2abd564430250b6caea18e35f
yousef <yabdelshafy0@gmail.com>
2023-04-10 15:02:46 +0200

    args

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,23 +1,22 @@
- #include <stdio.h>
-  #include <stdlib.h>
-   
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
-*Write a program that prints the number of arguments passed into it.
+*main - Write a program that prints the number of arguments passed into it.
 *@argc: counter of array.
 *@argv: array for string.
 *Return: return 0.
 */
 
-   int main (int argc, char** argv)
-   {
-           int i = 0;
-          
-	   if (i <= **argv)
-          {
-          printf("%d\n", argc);
-          i++;
-          }
-          return (0);
+int main(int argc, char **argv)
+{
+	int i = 0;
 
-      
-  }
+		if (i <= **argv)
+		{
+		printf("%d\n", argc - 1);
+		i++;
+		}
+		return (0);
+
+}
