f04bce5d31763ff0ad4a5201c599ac7c74b80884
yousef <yabdelshafy0@gmail.com>
2023-04-10 13:41:26 +0200

    name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main (int argc, char** argv)
+/**
+*main - Write a program that prints its name, followed by a new line.
+*@argc: counter of array.
+*@argv: array for string.
+*Return: return 0.
+*/
+int main(int argc, char **argv)
 {
 	int i;
+
 	for (i = 0; i < argc; i++)
 	{
 	printf("%s\n", *argv);
 	}
-	return 0;
+	return (0);
 }
