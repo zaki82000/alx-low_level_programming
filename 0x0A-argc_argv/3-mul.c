e8128355b1345ff9184bfa488a17e65c4b841f50
yousef <yabdelshafy0@gmail.com>
2023-04-11 02:05:44 +0200

    mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-   
+
 /**
 *main - program that multiplies two numbers,i: lenght array,
 *sum: multiplies two numbers.
 *@argc: lenght to array;
-*@arrgv: pointer to array string.
+*@argv: pointer to array string.
 *Return: return 1 is argc > 3 or < 1.
 */
 
@@ -14,15 +14,14 @@ int main(int argc, char **argv)
 
 	int i = 1, sum;
 
-		if (argc > 3 || argc < 3)
+		if (argc > i + 2 || argc < i + 2)
 		{
-		printf ("Error\n");
+		printf("Error\n");
 		return (1);
 		}
 
-	sum = atoi(argv[i]) * atoi(argv[i+1]);
+	sum = atoi(argv[i]) * atoi(argv[i + 1]);
 	printf("%d\n", sum);
 	return (0);
 
-}   
-
+}
