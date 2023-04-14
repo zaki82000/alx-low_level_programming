ea0376f4cee00d5f4e359c8321333febc83ef236
yousef <yabdelshafy0@gmail.com>
2023-04-14 03:31:14 +0200

    malloc check

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,13 +1,21 @@
 #include<stdlib.h>
 #include"main.h"
+
+/**
+*malloc_checked - function that allocates memory,
+*ptr:pointer to new space.
+*@b:length to varible.
+*Return: return pointer.
+*/
+
 void *malloc_checked(unsigned int b)
 {
-    void *ptr;
-    
-    ptr = malloc(b);
-    
-    if (ptr == NULL)
-    exit(98);
-    
-    return (ptr);
+		void *ptr;
+
+		ptr = malloc(b);
+
+		if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
 }
