7f9d61625c5fe5050f57f036fa0070adbcd515a7
yousef <yabdelshafy0@gmail.com>
2023-04-15 04:33:27 +0200

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,18 +9,20 @@
 *return NULL if pointer ptr eqail NULL in and,
 *return pointer ptr.
 */
-
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *ptr;
+	unsigned int i;
+
 		if (nmemb == 0 || size == 0)
 		return (NULL);
-
 		ptr = malloc(nmemb * size);
-
 		if (ptr == NULL)
 		return (NULL);
 
+		for (i = 0; i < (nmemb * size); i++)
+		{
+		((char *) ptr)[i] = 0;
+		}
 	return (ptr);
-
 }
