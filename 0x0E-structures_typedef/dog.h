538080a7602201873fcb13cd5bf58dc8fbb7a42c
yousef <yabdelshafy0@gmail.com>
2023-04-17 18:09:43 +0200

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,18 @@
 #ifndef DOG_H
-#define DOH_H
+#define DOG_H
+
+/**
+* struct dog - Define a new type dog.
+*@name: char type variable.
+*@age: float type variable.
+*@owner: char type variable.
+*/
 
 struct dog
 {
-    char * name;
-    float age;
-    char * owner;
+	char *name;
+	float age;
+	char *owner;
 };
 
 #endif
