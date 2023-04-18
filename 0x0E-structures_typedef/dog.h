b9587531a5586ee3be67bb814e5b264357a6d9d7
yousef <yabdelshafy0@gmail.com>
2023-04-18 18:17:05 +0200

    dog1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,12 +8,12 @@
 *@owner: char type variable.
 */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
 
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
