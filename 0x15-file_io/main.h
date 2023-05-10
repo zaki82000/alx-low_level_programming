9a37961e26d74e18c159a743c082575220dfa151
yousef <yabdelshafy0@gmail.com>
2023-05-10 03:21:52 +0300

    update main.h header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,6 +2,10 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
