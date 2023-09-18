c3703adbcf210b0445fd51b39b087434d6f7a9d9
yousef <yabdelshafy0@gmail.com>
2023-09-18 03:28:29 +0300

    add lists.h file

---
+++ 0x17-doubly_linked_lists/lists.h
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+typedef struct dlistint_t  {
+int n;
+struct dlistint_t* next;
+struct dlistint_t* prev;
+}dlistint_t;
+
+size_t print_dlistint(const dlistint_t *h);
